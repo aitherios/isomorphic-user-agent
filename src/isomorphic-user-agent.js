91ca485074f8d0a83467ccb6703e25a118c8b4e4
Renan Mendes Carvalho <aitherios@gmail.com>
2016-07-16 18:34:06 -0300

    Please the linter

--- src/isomorphic-user-agent.js
+++ src/isomorphic-user-agent.js
@@ -1,9 +1,13 @@
 import DefaultUserAgent from 'default-user-agent'
 
 export default () => {
-  if (typeof window !== 'undefined' && window !== null && window.navigator && window.navigator.userAgent) {
+  if (
+    typeof window !== 'undefined' &&
+    window !== null &&
+    window.navigator &&
+    window.navigator.userAgent
+  ) {
     return window.navigator.userAgent
-  } else {
-    return DefaultUserAgent()
   }
+  return DefaultUserAgent()
 }
